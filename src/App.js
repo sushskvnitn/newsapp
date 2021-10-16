ca3c8b37fab03b97b41128fd04a5c60956de8e20
Sushant Kogurwar <88105162+sushskvnitn@users.noreply.github.com>
2021-10-16 22:15:52 +0530

    removing router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState } from 'react'
 import NavBar from './components/Navbar';
 import News from './components/News';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+// import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
@@ -12,7 +12,7 @@ const App = ()=> {
  
     return (
       <div>
-        <Router>
+//         <Router>
         <NavBar/> 
         <LoadingBar
         height={3}
@@ -20,16 +20,16 @@ const App = ()=> {
         progress={progress} 
       />
         <Switch>
-          <Route exact path="/"><News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/business"><News setProgress={setProgress} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
-          <Route exact path="/entertainment"><News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></Route> 
-          <Route exact path="/general"><News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/health"><News setProgress={setProgress} key="health" pageSize={pageSize} country="in" category="health"/></Route> 
-          <Route exact path="/science"><News setProgress={setProgress} key="science" pageSize={pageSize} country="in" category="science"/></Route> 
-          <Route exact path="/sports"><News setProgress={setProgress} key="sports" pageSize={pageSize} country="in" category="sports"/></Route> 
-          <Route exact path="/technology"><News setProgress={setProgress} key="technology" pageSize={pageSize} country="in" category="technology"/></Route> 
+          <li  path="/"><News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/></li> 
+          <li  path="/business"><News setProgress={setProgress} key="business" pageSize={pageSize} country="in" category="business"/></li> 
+          <li  path="/entertainment"><News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></li> 
+          <li  path="/general"><News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general"/> </li> 
+          <li  path="/health"><News setProgress={setProgress} key="health" pageSize={pageSize} country="in" category="health"/></li> 
+          <li  path="/science"><News setProgress={setProgress} key="science" pageSize={pageSize} country="in" category="science"/> </li> 
+          <li path="/sports"><News setProgress={setProgress} key="sports" pageSize={pageSize} country="in" category="sports"/></li>  
+          <li  path="/technology"><News setProgress={setProgress} key="technology" pageSize={pageSize} country="in" category="technology"/> </li> 
         </Switch>
-        </Router>
+//         </Router>
       </div>
     )
  
